07b3556274f8c377468b53508287d374cceffeac
manti <manti@practo.com>
2015-10-16 17:13:13 +0530

    synatx error fix

--- Bot/Controllers/FedPostController.py
+++ Bot/Controllers/FedPostController.py
@@ -13,11 +13,8 @@ def fed_post():
 
     post_data = flask.request.get_json()
     if 'http' in post_data['raw'] or 'https' in post_data['raw']:
-	    response = requests.post(app.config['HOST']+'posts' + url, data=json.dumps(post_data), headers={"content-type":"application/json"})
-	    return response.json()
-	else:
-		return 'not valid url'
-
+		response = requests.post(app.config['HOST']+'posts' + url, data=json.dumps(post_data), headers={"content-type":"application/json"})
+		return response.json()
 def get_fed_post():
 	data = requests.get(app.config['HOST'] + 'latest.json' + url)
 	return data.json()['topic_list']['topics'][2]
